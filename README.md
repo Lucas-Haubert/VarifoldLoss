bf4241cfd6722214cee35074a5385bc9dcb5f623
Lucas-Haubert <lucas.haubert@ens-paris-saclay.fr>
2024-05-23 00:27:27 +0200

    naive and efficient dilate

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Flexible Deep Learning Time Series Forecasting framework.
 
-NB: This README file is more a rough draft than a real README. Now it stands to present to the supervisors my work. It will be reshaped soon. 
+NB: This README file is more a rough draft than a real README. Now it stands to present my work to the supervisors. It will be reshaped soon. 
 
 At this time, I have reproduced the experiments of the iTransformer repository (https://github.com/thuml/iTransformer/tree/main) from the server Ruche, I launched one baby experiment (1 epoch, run.py). 
 
 I also extended the evaluation metrics files, in order to integrate normalized versions and DTW.
 
-Next step: Focus on the losses. I have adapted the criterion function in order to train the network with either MSE or MAE. The gola is now to plug DILATE and TILDE-Q losses in the procedure, so that I can switch easily.
+Next step: Focus on the losses. I have adapted the criterion function in order to train the network with either MSE or MAE. The goal is now to plug DILATE and TILDE-Q losses in the procedure, so that I can switch easily.
--- loss/dilate/dilate_loss.py
+++ loss/dilate/dilate_loss.py
@@ -5,21 +5,24 @@ import path_soft_dtw
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def dilate_loss(outputs, targets, alpha, gamma, device):
-	# outputs, targets: shape (batch_size, forecast horizon, 1)
+
 	batch_size, N_output = outputs.shape[0:2]
+
 	loss_shape = 0
 	softdtw_batch = soft_dtw.SoftDTWBatch.apply
 	D = torch.zeros((batch_size, N_output,N_output )).to(device)
 	for k in range(batch_size):
-		Dk = soft_dtw.pairwise_distances(targets[k,:,:].view(-1,1),outputs[k,:,:].view(-1,1))
+		Dk = soft_dtw.pairwise_distances(targets[k,:,:],outputs[k,:,:])
 		D[k:k+1,:,:] = Dk     
 	loss_shape = softdtw_batch(D,gamma)
-	
+
 	path_dtw = path_soft_dtw.PathDTWBatch.apply
-	path = path_dtw(D,gamma)           
-	Omega =  soft_dtw.pairwise_distances(torch.range(1,N_output).view(N_output,1)).to(device)
+	path = path_dtw(D,gamma)       
+	Omega =  soft_dtw.pairwise_distances(torch.range(1,N_output).view(N_output,1)).to(device) 
 	loss_temporal =  torch.sum( path*Omega ) / (N_output*N_output) 
-	loss = alpha*loss_shape+ (1-alpha)*loss_temporal
+
+	loss = alpha * loss_shape + (1 - alpha) * loss_temporal
+
 	return loss, loss_shape, loss_temporal
 
 
@@ -42,32 +45,55 @@ def DILATE_multi_dim_naive(outputs, targets, alpha, gamma, device):
 
 import time
 
-pred_channel = torch.randn(2, 10, 3)
-true_channel = torch.randn(2, 10, 3)
+outputs = torch.randn(2, 10, 3)
+targets = torch.randn(2, 10, 3)
 
-print("pred_channel:")
-print(pred_channel)
+print("outputs:")
+print(outputs)
 
-print("\ntrue_channel")
-print(true_channel)
+print("\ntargets")
+print(targets)
 
 
 alpha = 0.5
 gamma = 0.5
 
 
-print("\nDILATE_multi_dim_naive(pred, true) with alpha =", alpha, " and gamma =", gamma)
+print("\nDILATE_multi_dim_naive(outputs, targets) with alpha =", alpha, " and gamma =", gamma)
 start_time_naive = time.time()
-loss_naive = DILATE_multi_dim_naive(pred_channel, true_channel, alpha, gamma, device)
+loss_naive = DILATE_multi_dim_naive(outputs, targets, alpha, gamma, device)
 end_time_naive = time.time()
 duration_naive = end_time_naive - start_time_naive
 print("Loss naive:", loss_naive)
 print("Duration naive:", duration_naive, "seconds")
 
-"""print("\nDILATE_multi_dim_efficient(pred, true) with alpha =", alpha, " and gamma =", gamma)
-start_time_efficient = time.time()
-loss_efficient = DILATE_multi_dim_efficient(pred_channel, true_channel, alpha, gamma, device)
-end_time_efficient = time.time()
-duration_efficient = end_time_efficient - start_time_efficient
-print("Loss efficient:", loss_efficient)
-print("Duration efficient:", duration_efficient, "seconds")"""
+print("\ndilate_loss(outputs, targets) with alpha =", alpha, " and gamma =", gamma)
+start_time_dilate_loss = time.time()
+loss_dilate_loss = dilate_loss(outputs, targets, alpha, gamma, device)
+end_time_dilate_loss = time.time()
+duration_dilate_loss = end_time_dilate_loss - start_time_dilate_loss
+print("Loss dilate original:", loss_dilate_loss)
+print("Duration dilate original:", duration_dilate_loss, "seconds")
+
+
+
+"""
+print("Pairwise matrix on multivariate time series")
+batch_size, N_output = outputs.shape[0:2]
+D = torch.zeros((batch_size, N_output, N_output)).to(device)
+for k in range(batch_size):
+	Dk = soft_dtw.pairwise_distances(outputs[k,:,:],targets[k,:,:])
+	D[k:k+1,:,:] = Dk  
+print(D)   
+softdtw_batch = soft_dtw.SoftDTWBatch.apply
+print("Loss shape")
+loss_shape = softdtw_batch(D,gamma)
+print(loss_shape)"""
+
+
+"""
+print("\ndilate_loss(outputs, targets, alpha, gamma, device) with alpha =", alpha, " and gamma =", gamma)
+loss, loss_shape, loss_temporal = dilate_loss(pred_channel, true_channel, alpha, gamma, device)
+print(loss.item())
+"""
+
