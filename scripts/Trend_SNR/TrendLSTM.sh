c4b1d564f777d7e25716a260035ce9209ec0651f
Lucas-Haubert <lucas.haubert@ens-paris-saclay.fr>
2024-09-18 12:26:46 +0200

    update

---
+++ scripts/Trend_SNR/TrendLSTM.sh
@@ -0,0 +1,245 @@
+#!/bin/bash
+
+model_name=TrendLSTM
+
+snr_values=( 20 15 10 5 )
+
+for snr in "${snr_values[@]}"
+do
+    script_name_str="Rob_Trend_TrendLSTM_MSE"
+    
+    python -u run.py \
+        --is_training 1 \
+        --root_path ./dataset/synthetic/ \
+        --data_path Trend_SNR_${snr}.csv \
+        --structural_data_path Trend_SNR_infty.csv \
+        --evaluation_mode 'structural' \
+        --script_name $script_name_str \
+        --model $model_name \
+        --loss 'MSE' \
+        --train_epochs 20 \
+        --patience 5 \
+        --data custom \
+        --features S \
+        --target value \
+        --seq_len 96 \
+        --pred_len 96 \
+        --enc_in 1 \
+        --d_model 256 \
+        --e_layers 3 \
+        --des 'Exp' \
+        --batch_size 4 \
+        --learning_rate 0.0001 \
+        --itr 5
+done
+
+
+for snr in "${snr_values[@]}"
+do
+    script_name_str="Rob_Trend_TrendLSTM_DILATE"
+    
+    python -u run.py \
+        --is_training 1 \
+        --root_path ./dataset/synthetic/ \
+        --data_path Trend_SNR_${snr}.csv \
+        --structural_data_path Trend_SNR_infty.csv \
+        --evaluation_mode 'structural' \
+        --script_name $script_name_str \
+        --model $model_name \
+        --loss 'DILATE' \
+        --alpha_dilate 0.05 \
+        --gamma_dilate 0.1 \
+        --train_epochs 20 \
+        --patience 5 \
+        --data custom \
+        --features S \
+        --target value \
+        --seq_len 96 \
+        --pred_len 96 \
+        --enc_in 1 \
+        --d_model 256 \
+        --e_layers 3 \
+        --des 'Exp' \
+        --batch_size 4 \
+        --learning_rate 0.0001 \
+        --itr 5
+done
+
+
+gamma_values=( 0.0001 0.001 0.01 0.1 )
+
+for gamma in "${gamma_values[@]}"
+do
+    for snr in "${snr_values[@]}"
+    do
+        script_name_str="Rob_Trend_TrendLSTM_Soft_DTW"
+        
+        python -u run.py \
+            --is_training 1 \
+            --root_path ./dataset/synthetic/ \
+            --data_path Trend_SNR_${snr}.csv \
+            --structural_data_path Trend_SNR_infty.csv \
+            --evaluation_mode 'structural' \
+            --script_name $script_name_str \
+            --model $model_name \
+            --loss 'DILATE' \
+            --alpha_dilate 1 \
+            --gamma_dilate $gamma \
+            --train_epochs 20 \
+            --patience 5 \
+            --data custom \
+            --features S \
+            --target value \
+            --seq_len 96 \
+            --pred_len 96 \
+            --enc_in 1 \
+            --d_model 256 \
+            --e_layers 3 \
+            --des 'Exp' \
+            --batch_size 4 \
+            --learning_rate 0.0001 \
+            --itr 5
+    done
+done
+
+
+for snr in "${snr_values[@]}"
+do
+    script_name_str="Rob_Trend_TrendLSTM_Distrib"
+    
+    python -u run.py \
+        --is_training 1 \
+        --root_path ./dataset/synthetic/ \
+        --data_path Trend_SNR_${snr}.csv \
+        --structural_data_path Trend_SNR_infty.csv \
+        --evaluation_mode 'structural' \
+        --script_name $script_name_str \
+        --model $model_name \
+        --loss 'VARIFOLD' \
+        --position_kernel 'Gaussian' \
+        --sigma_t_pos 1 \
+        --sigma_s_pos 16 \
+        --orientation_kernel 'Distribution' \
+        --train_epochs 20 \
+        --patience 5 \
+        --data custom \
+        --features S \
+        --target value \
+        --seq_len 96 \
+        --pred_len 96 \
+        --enc_in 1 \
+        --d_model 256 \
+        --e_layers 3 \
+        --des 'Exp' \
+        --batch_size 4 \
+        --learning_rate 0.0001 \
+        --itr 5
+done
+
+
+for snr in "${snr_values[@]}"
+do
+    script_name_str="Rob_Trend_TrendLSTM_Current"
+
+    python -u run.py \
+        --is_training 1 \
+        --root_path ./dataset/synthetic/ \
+        --data_path Trend_SNR_${snr}.csv \
+        --structural_data_path Trend_SNR_infty.csv \
+        --evaluation_mode 'structural' \
+        --script_name $script_name_str \
+        --model $model_name \
+        --loss 'VARIFOLD' \
+        --position_kernel 'Gaussian' \
+        --sigma_t_pos 1 \
+        --sigma_s_pos 16 \
+        --orientation_kernel 'Current' \
+        --sigma_t_or 1 \
+        --sigma_s_or 5 \
+        --train_epochs 20 \
+        --patience 5 \
+        --data custom \
+        --features S \
+        --target value \
+        --seq_len 96 \
+        --pred_len 96 \
+        --enc_in 1 \
+        --d_model 256 \
+        --e_layers 3 \
+        --des 'Exp' \
+        --batch_size 4 \
+        --learning_rate 0.0001 \
+        --itr 5
+done
+
+
+for snr in "${snr_values[@]}"
+do
+    script_name_str="Rob_Trend_TrendLSTM_UnorVar"
+
+    python -u run.py \
+        --is_training 1 \
+        --root_path ./dataset/synthetic/ \
+        --data_path Trend_SNR_${snr}.csv \
+        --structural_data_path Trend_SNR_infty.csv \
+        --evaluation_mode 'structural' \
+        --script_name $script_name_str \
+        --model $model_name \
+        --loss 'VARIFOLD' \
+        --position_kernel 'Gaussian' \
+        --sigma_t_pos 1 \
+        --sigma_s_pos 16 \
+        --orientation_kernel 'UnorientedVarifold' \
+        --sigma_t_or 1 \
+        --sigma_s_or 10 \
+        --train_epochs 20 \
+        --patience 5 \
+        --data custom \
+        --features S \
+        --target value \
+        --seq_len 96 \
+        --pred_len 96 \
+        --enc_in 1 \
+        --d_model 256 \
+        --e_layers 3 \
+        --des 'Exp' \
+        --batch_size 4 \
+        --learning_rate 0.0001 \
+        --itr 5
+done
+
+
+for snr in "${snr_values[@]}"
+do
+    script_name_str="Rob_Trend_TrendLSTM_OrVar"
+
+    python -u run.py \
+        --is_training 1 \
+        --root_path ./dataset/synthetic/ \
+        --data_path Trend_SNR_${snr}.csv \
+        --structural_data_path Trend_SNR_infty.csv \
+        --evaluation_mode 'structural' \
+        --script_name $script_name_str \
+        --model $model_name \
+        --loss 'VARIFOLD' \
+        --position_kernel 'Gaussian' \
+        --sigma_t_pos 1 \
+        --sigma_s_pos 16 \
+        --orientation_kernel 'OrientedVarifold' \
+        --sigma_t_or 1000 \
+        --sigma_s_or 10 \
+        --train_epochs 20 \
+        --patience 5 \
+        --data custom \
+        --features S \
+        --target value \
+        --seq_len 96 \
+        --pred_len 96 \
+        --enc_in 1 \
+        --d_model 256 \
+        --e_layers 3 \
+        --des 'Exp' \
+        --batch_size 4 \
+        --learning_rate 0.0001 \
+        --itr 5
+done
